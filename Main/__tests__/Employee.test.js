067b660fa34f17f0705091678f604d1a6d858d00
Jay Jenks <jamesljenks@gmail.com>
2021-06-03 22:35:34 -0400

    Attempting to correct error.

---
+++ Main/__tests__/Employee.test.js
@@ -0,0 +1,51 @@
+const Employee = require("../lib/Employee");
+
+test("Can create an new employee.", () => {
+    const employeeInstance = new Employee();
+    expect(typeof(employeeInstance)).toBe("object");
+})
+
+test("Testing name.", () => {
+    const name = "James";
+    const employeeInstance = new Employee(name);
+    expect(employeeInstance.name).toBe(name);
+})
+
+test("Testing ID.", () => {
+    const id = 2;
+    const employeeInstance = new Employee("James", id);
+    expect(employeeInstance.id).toBe(id);
+})
+
+test("Testing email.", () => {
+    const email = "jamesljenks@gmail.com";
+    const employeeInstance = new Employee("James", 2, email);
+    expect(employeeInstance.email).toBe(email);
+})
+
+
+
+test("Gets name through getName method.", () => {
+    const testName = "James";
+    const employeeInstance = new Employee(testName);
+    expect(employeeInstance.getName()).toBe(testName);
+})
+
+test("Can test ID through getID method.", () => {
+    const testID = 2;
+    const employeeInstance = new Employee("James", testID);
+    expect(employeeInstance.getID()).toBe(testID);
+})
+
+test("Can test email through getEmail method.", () => {
+    const testEmail = "jamesljenks@gmail.com";
+    const employeeInstance = new Employee("James", 2, testEmail);
+    expect(employeeInstance.getEmail()).toBe(testEmail);
+})
+
+test("Testing role.", () => {
+    const returnValue = "Employee";
+    const employeeInstance = new Employee("James", 2, "jamesljenks@gmail.com");
+    expect(employeeInstance.getRole()).toBe(returnValue);
+})
+

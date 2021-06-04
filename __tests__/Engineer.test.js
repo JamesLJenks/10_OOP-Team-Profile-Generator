cab2cfb35c44f52f9eb68c393f91ad4007eb725a
Jay Jenks <jamesljenks@gmail.com>
2021-06-04 00:15:04 -0400

    Started coding index.js.

---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,19 @@
+const Engineer = require("../lib/Engineer");
+
+test("Can create a github.", () => {
+    const testGithub = "JamesLJenks";
+    const employeeInstance = new Engineer("James", 2, "jamesljenks@gmail.com", testGithub);
+    expect(employeeInstance.github).toBe(testGithub);
+})
+
+test("Testing getGithub will return github.", () => {
+    const testGithub = "JamesLJenks";
+    const employeeInstance = new Engineer("James", 2, "jamesljenks@gmail.com", testGithub);
+    expect(employeeInstance.getGithub()).toBe(testGithub);
+})
+
+test("Testing role.", () => {
+    const returnValue = "Engineer";
+    const employeeInstance = new Engineer("James", 2, "jamesljenks@gmail.com", "JamesLJenks");
+    expect(employeeInstance.getRole()).toBe(returnValue);
+})
---
+++ index.js
@@ -0,0 +1,64 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+
+teamArray = [];
+
+
+
+
+
+
+
+
+function runApp () {
+// Add OOP functions here - create for the other roles, but not Employee.
+
+function addEngineer() {
+    inquirer.prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is your engineer's name?",
+        
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is your engineer's id?",
+        
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is your engineer's email?",
+        
+      },
+      {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is your engineer's GitHub username?",
+        
+      }
+    ]).then(answers => {
+      const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+      teamArray.push(engineer)
+      createTeam();
+    });
+  }
+
+
+
+
+
+function createTeam () {
+    console.log("Team created!")
+}
+
+}

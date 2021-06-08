aba3bd335ef13e17dc887f5f3d7dbc7f0380ea22
Jay Jenks <jamesljenks@gmail.com>
2021-06-07 22:41:17 -0400

    Added the addManager and addIntern functions to the index.js file.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 
@@ -18,43 +18,121 @@ teamArray = [];
 
 
 function runApp () {
-// Add OOP functions here - create for the other roles, but not Employee (Do for Intern and Manager)
+
+// OOP Functions
+
+function addManager() {
+  inquirer.prompt ([
+    
+    {
+      type: "input",
+      name: "managerName",
+      message: "What is the manager's name?"
+    },
+
+    {
+      type: "input",
+      name: "managerId",
+      message: "What is the manager's employee ID number?"
+    },
+
+    {
+      type: "input",
+      name: "managerEmail",
+      message: "What is the manager's email address?"
+    },
+
+    {
+      type: "input",
+      name: "managerPhone",
+      message: "What is the manager's office phone number?"
+    }
+
+  ]).then(answers => {
+    const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerPhone);
+    teamArray.push(manager);
+    createTeam();
+  });
+
+}
+
 
 function addEngineer() {
     inquirer.prompt([
+      
       {
         type: "input",
         name: "engineerName",
-        message: "What is your engineer's name?",
-        
+        message: "What is the engineer's name?"
       },
+
       {
         type: "input",
-        name: "engineerId",
-        message: "What is your engineer's id?",
-        
+        name: "engineerId", // should ID numbers be auto-assigned?
+        message: "What is the engineer's employee ID number?" 
       },
+
       {
         type: "input",
         name: "engineerEmail",
-        message: "What is your engineer's email?",
-        
+        message: "What is the engineer's email address?"
       },
+
       {
         type: "input",
         name: "engineerGithub",
-        message: "What is your engineer's GitHub username?",
-        
+        message: "What is the engineer's GitHub username?"
       }
+
     ]).then(answers => {
       const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
-      teamArray.push(engineer)
+      teamArray.push(engineer);
       createTeam();
     });
+
   }
 
+  function addIntern() {
+    inquirer.prompt([
+      
+      {
+        type: "input",
+        name: "internName",
+        message: "What is the intern's name?"
+      },
 
+      {
+        type: "input",
+        name: "internId",
+        message: "What is the intern's employee ID number?" 
+      },
 
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is the intern's email address?"
+      },
+
+      {
+        type: "input",
+        name: "internSchool",
+        message: "What school does the intern attend?"
+      }
+
+    ]).then(answers => {
+      const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+      teamArray.push(intern);
+      createTeam();
+    });
+
+  }
+
+  // function to return to menu with option to add more team members
+
+  // Would you like to add another team member?
+  // Yes || No
+  // Yes --> Select a role for your new team member: Manager, Engineer, Intern
+  // No --> Create Team
 
 
 function createTeam () {
